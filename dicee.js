a07a83801f3f55286dab6221900edbd4654df6a6
OnasPrezdam2020 <64226965+OnasPrezdam2020@users.noreply.github.com>
2021-06-04 05:10:35 -0700

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,5 +1,5 @@
 var myPix = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
-var myPictureList = Array.from(document.getElementsByClassName(".img1"));
+var myPictureList = Array.from(document.getElementsByClassName("img1"));
 function pixGenerator() {
 
   var randomNumber1 = Math.floor(Math.random() * myPix.length);
