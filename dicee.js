68c994f8f67677483b501dd841085b43a175c3a7
OnasPrezdam2020 <64226965+OnasPrezdam2020@users.noreply.github.com>
2021-06-04 04:56:31 -0700

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,14 @@
+var myPix = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+var myPictureList = Array.from(document.getElementsByClassName(".img1"));
+function pixGenerator() {
+
+  var randomNumber1 = Math.floor(Math.random() * myPix.length);
+
+  var finalResult = myPix[randomNumber1];
+  myPictureList.forEach(function(pic){
+    pic.src = finalResult;
+  })
+
+}
+
+pixGenerator();
